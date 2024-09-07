d0c38b712e0f2758d5cc1a4465b77816b1084fbf
Alejandro Muñoz <esneideralejandro17@gmail.com>
2024-09-07 08:33:00 -0500

    Menu

---
+++ lista_compras.py
@@ -0,0 +1,10 @@
+lista_compras = []
+
+while true:
+    print("Bienvenido, a continuación se mostrará nuestro menú de opciones:")
+    print("1. Agregar artículo")
+    print("2. Eliminar artículo")
+    print("3. Mostrar lista")
+    print("4. Salir")
+
+    
